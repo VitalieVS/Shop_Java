e75665e4df62854372eb2301cdd4d77e618d0a2c
VitalieVS <vitaliesandiuc@gmail.com>
2021-08-29 15:57:47 +0300

    Changed from activity to fragments. Working perfectly.

--- app/src/main/java/com/example/shop_java/MainActivity.java
+++ app/src/main/java/com/example/shop_java/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.shop_java;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
+
+import com.example.shop_java.menu_fragments.CartFragment;
+import com.example.shop_java.menu_fragments.HomeFragment;
+import com.example.shop_java.menu_fragments.LoginFragment;
+import com.example.shop_java.menu_fragments.ReviewsFragment;
+import com.example.shop_java.menu_fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.Objects;
 
@@ -18,5 +28,45 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setSelectedItemId(R.id.home);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment selectedFragment;
+
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case R.id.search:
+                        selectedFragment = new SearchFragment();
+                        break;
+                    case R.id.reviews:
+                        selectedFragment = new ReviewsFragment();
+                        break;
+                    case R.id.login:
+                        selectedFragment = new LoginFragment();
+                        break;
+                    case R.id.cart:
+                        selectedFragment = new CartFragment();
+                        break;
+                    default:
+                        throw new UnknownError("Unknown value");
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        selectedFragment).commit();
+
+                return true;
+            }
+        });
     }
 }
