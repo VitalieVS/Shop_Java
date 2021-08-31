bfdb19997c7d8dd170617bdf9d6cd19d31342d61
VitalieVS <vitaliesandiuc@gmail.com>
2021-08-31 12:30:42 +0300

    Data binding on recyclerview

--- app/src/main/java/com/example/shop_java/promotion/ui/PromotionsAdapter.java
+++ app/src/main/java/com/example/shop_java/promotion/ui/PromotionsAdapter.java
@@ -9,6 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.shop_java.R;
+import com.example.shop_java.databinding.PromotionItemBinding;
 import com.example.shop_java.promotion.model.PromotionModel;
 
 import java.util.ArrayList;
@@ -20,20 +21,24 @@ public class PromotionsAdapter extends RecyclerView.Adapter<PromotionsAdapter.Pr
     @NonNull
     @Override
     public PromotionsAdapter.PromotionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return new PromotionViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.promotion_item, parent, false));
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+
+        PromotionItemBinding promotionItemBinding = PromotionItemBinding.inflate(layoutInflater, parent, false);
+
+
+        return new PromotionViewHolder(promotionItemBinding);
+
+        //PromotionViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.promotion_item, parent, false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull PromotionsAdapter.PromotionViewHolder holder, int position) {
-        holder.promo_id.setText("try");
-        holder.promo_title.setText("again");
-        holder.promo_body.setText("another one");
+        PromotionModel promotion = promotionList.get(position);
+        holder.promotionItemBinding.setViewModel(promotion);
     }
 
     @Override
     public int getItemCount() {
-        System.out.println("SIZE:");
-        System.out.println(promotionList.size());
         return promotionList.size();
     }
 
@@ -43,13 +48,13 @@ public class PromotionsAdapter extends RecyclerView.Adapter<PromotionsAdapter.Pr
     }
 
     public class PromotionViewHolder extends RecyclerView.ViewHolder {
-        TextView promo_id, promo_title, promo_body;
 
-        public PromotionViewHolder(@NonNull View itemView) {
-            super(itemView);
-            promo_id = itemView.findViewById(R.id.promo_id);
-            promo_title = itemView.findViewById(R.id.promo_title);
-            promo_body = itemView.findViewById(R.id.promo_body);
+        PromotionItemBinding promotionItemBinding;
+
+
+        public PromotionViewHolder(@NonNull PromotionItemBinding promotionItemBinding) {
+            super(promotionItemBinding.getRoot());
+            this.promotionItemBinding = promotionItemBinding;
         }
     }
 }
