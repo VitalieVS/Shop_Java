ea602ae0c56a22521bf1d8ed821e79d44add741e
VitalieVS <vitaliesandiuc@gmail.com>
2021-08-31 12:02:56 +0300

    Fix on throwable.

--- app/src/main/java/com/example/shop_java/promotion/ui/PromotionViewModel.java
+++ app/src/main/java/com/example/shop_java/promotion/ui/PromotionViewModel.java
@@ -14,19 +14,16 @@ import retrofit2.Response;
 
 public class PromotionViewModel extends ViewModel {
     public MutableLiveData<List<PromotionModel>> promotionsMutableLiveData = new MutableLiveData<>();
-    MutableLiveData<String> posts = new MutableLiveData<>();
     public void getPromotions() {
         PromotionsClient.getInstance().getPromotions().enqueue(new Callback<List<PromotionModel>>() {
             @Override
             public void onResponse(Call<List<PromotionModel>> call, Response<List<PromotionModel>> response) {
-                System.out.println("RESPONSE:");
-                System.out.println(response);
                 promotionsMutableLiveData.setValue(response.body());
             }
 
             @Override
             public void onFailure(Call<List<PromotionModel>> call, Throwable t) {
-                posts.setValue("EROR IN PULA MEA");
+                throw new UnknownError("Internet may be missing!");
             }
         });
     }
